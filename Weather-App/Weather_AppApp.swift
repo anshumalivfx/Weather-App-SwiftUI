a6db53bee05834c9cf6f2f227820fdf84b0e0c21
anshumalivfx <anshumali.karna99@gmail.com>
2022-02-02 11:42:31 +0530

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Anshumali Karna on 02/02/22.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
